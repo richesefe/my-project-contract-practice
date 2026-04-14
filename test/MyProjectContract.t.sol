8f6e15eb71361ba5c410313d8f5dd05921379fae
riches <goldriches805@gmail.com>
2026-04-14 07:43:24 +0100

    Created My Project Contract,tested and deployed

---
+++ test/MyProjectContract.t.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "forge-std/Test.sol";
+import "../src/MyProjectContract.sol";
+
+contract MyProjectContractTest is Test {
+    MyProjectContract projectContract;
+
+    function setUp() public {
+        projectContract = new MyProjectContract();
+    }
+ function testInitialValueIsZero() public {
+    uint256  value =  projectContract.get();
+    assertEq(value, 0);
+ }
+
+ function testSetValue() public {
+    projectContract.set(10);
+    assertEq(projectContract.get(),10);
+ }
+}
